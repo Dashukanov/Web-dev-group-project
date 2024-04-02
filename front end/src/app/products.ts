5be1382e90938856c4729fabd03275fca0cdd8d1
Dastan Shukanov <156771986+Dashukanov@users.noreply.github.com>
2024-04-03 01:58:32 +0500

    template

---
+++ front end/src/app/products.ts
@@ -0,0 +1,89 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Movie 1',
+    price: 631490 ,
+    description: 'description1',
+    rating:4.2,
+    imageSrc: 'https://kinogo.biz/uploads/mini/minifull/db9/1709064741-805731091.webp',
+    movieLink: 'https://kinogo.biz/109556-djuna-chast-vtoraja.html',
+    prodcategory: "1",
+    shows: true,
+    likes:0
+  },
+  {
+    id: 2,
+    name: 'movie 2',
+    price: 294534,
+    description: 'desc 2',
+    rating:4.1,
+    imageSrc: 'https://kinogo.biz/uploads/mini/minifull/2a1/1707470186-439685377.webp',
+    movieLink: 'https://kinogo.biz/109074-iz-glubiny.html',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+
+  {
+    id: 8,
+    name: 'movie 1',
+    price: 127982,
+    description: 'desc 1',
+    rating:4.5,
+    imageSrc: 'https://kinogo.biz/uploads/mini/minifull/45f/1681977988-1242508913.webp',
+    movieLink: 'https://kinogo.biz/56168-pila.html',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 9,
+    name: 'movie 2',
+    price: 454,
+    description: 'desc 2',
+    rating:4.5,
+    imageSrc: 'https://kinogo.biz/uploads/mini/minifull/1c5/1585497615-1688097823.webp',
+    movieLink: 'https://kinogo.biz/25060-pila-6.html',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+
+  {
+    id: 13,
+    name: 'movie 1',
+    price: 431902,
+    description: 'desc 1',
+    rating:4.5,
+    imageSrc: 'https://kinogo.biz/uploads/mini/minifull/fdb/1689960517-1577273899.webp',
+    movieLink: 'https://kinogo.biz/56955-barbi-2023.html',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 14,
+    name: 'movie 2',
+    price: 119990,
+    description: 'desc 2',
+    rating:4.6,
+    imageSrc: 'https://kinogo.biz/uploads/mini/minifull/438/1585415901-1259270427.webp',
+    movieLink: 'https://kinogo.biz/17793-la-la-lend.html',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+
+  {
+    id: 18,
+    name: 'movie 1',
+    price: 49839,
+    description: 'desc 1',
+    rating:3,
+    imageSrc: 'https://kinogo.biz/uploads/mini/minifull/c4b/1585406831-421818509.webp',
+    movieLink: 'https://kinogo.biz/16512-sem.html',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  },
+];
